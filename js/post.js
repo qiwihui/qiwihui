82de7fb4aaed836b229ada50b7a56f9f6a5b4db7
Qiwihui <qwh005007@gmail.com>
2014-06-28 20:12:14 -0700

    Add disqus click

--- js/post.js
+++ js/post.js
@@ -36,6 +36,14 @@ $(document).ready(function(){
             }
         }
     });
+	
+	window.disqus_shortname = 'blogdaozhanginfo'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('Loading...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
+
 
     (function(){
         var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
